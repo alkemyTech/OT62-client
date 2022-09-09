3d6303e4a8741a92313d96c1e574b2ebc4e14cf2
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2022-09-09 17:06:55 -0300

    added slides mocks

---
+++ src/mocks/slidesMock.js
@@ -0,0 +1,16 @@
+const Sliders = [
+    {
+        name: 'Slider numero 1',
+        image: 'https://images.unsplash.com/photo-1508779544523-dd1b27685be3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        name: 'Slider numero 2',
+        image: 'https://seekewa.com/public/front/images/blog-img-01.jpg'
+    },
+    {
+        name: 'Slider numero 3',
+        image: 'https://espaciopymes.com/wp-content/uploads/2018/12/shutterstock_511307368-p10869-m53827-800x533_w100.jpg'
+    },
+]
+
+export default Sliders;
