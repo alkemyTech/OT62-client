05f060cb34df79cea983cb839b56e0e0a5a9f9d9
leandro <martinezleandro2004@gmail.com>
2022-09-12 13:05:15 -0300

    pagina de contacto añadida

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Public from './pages/Public/Public.js';
 import Backoffice from './pages/Backoffice/Backoffice.js';
+import Contacts from './pages/Public/Contacts.js';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Public />}/>
-          <Route path='/backoffice/' element={<Backoffice />}/>
+          <Route path='/' element={<Public />} />
+          <Route path='/contacts' element={<Contacts />} />
+          <Route path='/backoffice/' element={<Backoffice />} />
         </Routes>
       </Router>
     </>
