f56fd0b7c81129566d91dcda9090ccfe1eed8592
leandro <martinezleandro2004@gmail.com>
2022-09-12 13:17:53 -0300

    pagina de nosotros añadida

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Public from './pages/Public/Public.js';
 import Backoffice from './pages/Backoffice/Backoffice.js';
 import Contacts from './pages/Public/Contacts.js';
+import Nosotros from './pages/Public/Nosotros.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Public />} />
           <Route path='/contacts' element={<Contacts />} />
+          <Route path='/nosotros' element={<Nosotros />} />
           <Route path='/backoffice/' element={<Backoffice />} />
         </Routes>
       </Router>
