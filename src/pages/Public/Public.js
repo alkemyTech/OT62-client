77cc1193500637909eeb5c057f60fb8d28436efc
leandro <martinezleandro2004@gmail.com>
2022-09-13 17:56:48 -0300

    footer

--- src/pages/Public/Public.js
+++ src/pages/Public/Public.js
@@ -11,10 +11,10 @@ const Public = () => {
         <>
             {/* Header */}
             <Routes>
-                <Route path="/" element={<h1>Public Routes</h1>}/>
-                <Route path="/news" element={<NewsList />}/>
-                <Route path="/news/:id" element={<NewsDetail />}/>
-                <Route path="/actividades/:id" element={<ActivityDetail />}/>
+                <Route path="/" element={<h1>Public Routes</h1>} />
+                <Route path="/news" element={<NewsList />} />
+                <Route path="/news/:id" element={<NewsDetail />} />
+                <Route path="/actividades/:id" element={<ActivityDetail />} />
                 <Route path='/contacts' element={<Contacts />} />
                 <Route path='/nosotros' element={<Nosotros />} />
             </Routes>
