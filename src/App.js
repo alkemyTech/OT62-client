7fe3e5fe41afa4de8048a54950f5d40e4771bb0c
marian.eq <mariano.eq@gmail.com>
2022-09-14 23:09:59 +0200

    Login and register form created

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
-import React from 'react';
-
-
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import SignUpContainer from './pages/SignUp/Signup.container';
-import LoginContainer from './pages/Login/Login.container';
-import Public from './pages/Public/Public.js';
-import Backoffice from './pages/Backoffice/Backoffice.js';
-import Contacts from './pages/Public/Contacts.js';
-import Nosotros from './pages/Public/Nosotros.js';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import SignUpContainer from "./pages/SignUp/Signup.container";
+import LoginContainer from "./pages/Login/Login.container";
+import Public from "./pages/Public/Public.js";
+import Backoffice from "./pages/Backoffice/Backoffice.js";
+import Contacts from "./pages/Public/Contacts.js";
+import Nosotros from "./pages/Public/Nosotros.js";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path="/create-activity" component={ActivitiesForm} />
-          <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/create-user" component={UserForm} />
-          <Route path="/register-user" component={SignUpContainer} />
-          <Route path="/login-user" component={LoginContainer} />
-          <Route path="/create-member" component={MembersForm} />
-          <Route path="/create-project" component={ProjectsForm} />
-          <Route path="/school-campaign" component={SchoolCampaign} />
-          <Route path="/toys-campaign" component={ToysCampaign} />
-        </Switch>
-      </BrowserRouter>
-    
       <Router>
         <Routes>
-          <Route path='/' element={<Public />} />
-          <Route path='/contacts' element={<Contacts />} />
-          <Route path='/nosotros' element={<Nosotros />} />
-          <Route path='/backoffice/' element={<Backoffice />} />
+          <Route path="/create-activity" element={<ActivitiesForm />} />
+          <Route path="/create-category" element={<CategoriesForm />} />
+          <Route path="/create-news" element={<NewsForm />} />
+          <Route path="/backoffice/create-slide" element={<SlidesForm />} />
+          <Route path="/create-testimonials" element={<TestimonialForm />} />
+          <Route path="/create-user" element={<UserForm />} />
+          <Route path="/register-user" element={<SignUpContainer />} />
+          <Route path="/login-user" element={<LoginContainer />} />
+          <Route path="/create-member" element={<MembersForm />} />
+          <Route path="/create-project" element={<ProjectsForm />} />
+          <Route path="/school-campaign" element={<SchoolCampaign />} />
+          <Route path="/toys-campaign" element={<ToysCampaign />} />
+          <Route path="/" element={<Public />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/backoffice/" element={<Backoffice />} />
         </Routes>
       </Router>
     </>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,21 +3,23 @@ import { Field, Form } from "formik";
 import { Link } from "react-router-dom";
 function Login() {
   return (
-    <div>
-      <div>
+    <div class="border-8 flex flex-col justify-center h-screen my-0 mx-auto">
+      <div class="w-2/4 flex flex-col items-center">
         <h6>Bienvenido</h6>
         <h3>Inicia sesión en tu cuenta!</h3>
-      </div>
-      <div>
+
         <Form>
-          <div>
-            <label>Email</label>
-            <Field id="email" name="email" type="email" />
+          <div class="border border-solid rounded-lg border-gray-400">
+            <Field id="email" name="email" type="email" placeholder="Email" />
           </div>
 
           <div>
-            <label>Password</label>
-            <Field id="password" name="password" type="password" />
+            <Field
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Contraseña"
+            />
           </div>
           <div>
             <button type="submit">Submit</button>
@@ -25,7 +27,7 @@ function Login() {
         </Form>
       </div>
       <p>
-        No tienes una cuenta? <Link to="./register-user">Registrate</Link>
+        No tienes una cuenta? <Link to="/register-user">Registrate</Link>
       </p>
     </div>
   );
