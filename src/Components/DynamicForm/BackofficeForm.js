5954cb4b4d5963ac312840ac5643d7a33c3cd860
Gonzalo <espindolamgonzalo@gmail.com>
2022-09-18 00:39:35 -0300

    added create and edit dynamic form

---
+++ src/Components/DynamicForm/BackofficeForm.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Form, Formik } from "formik";
+import valueTranslate from "../utils/valueTranslate";
+import APICalls from "../../shared/APICalls";
+import { ContentField, TextField } from "./FieldsType";
+
+const BackofficeForm  = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const {fields, method, route, title } = location.state;
+
+    if (location.state.data) {
+        var data = location.state.data;
+    }
+
+    const onSubmit = async (values) => {
+        switch (method) {
+            case 'POST':
+                await APICalls.post(`/${route}`, values)
+                break;
+            case 'PUT':
+                await APICalls.put(`/${route}/${data.id}`, values)
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <>
+            <Formik
+                initialValues={data ? data : fields}
+                onSubmit={onSubmit}
+            >
+                {({ handleChange, handleSubmit }) => (
+                    <div className="flex justify-center h-full">
+                        <Form onSubmit={handleSubmit} className="flex flex-col w-3/4 items-center p-6 border-[1px] shadow-lg m-7">
+                            <h1 className="text-3xl mb-4 font-semibold">{title}</h1>
+                            {Object.entries(fields).map((value, index) => {
+                                return (
+                                    <>
+                                        <label key={index} className="font-semibold mb-1">{valueTranslate(value, 'label')}</label>
+                                        {generateInputs(value, handleChange)}
+                                    </>
+                                )
+                            })}
+                            <div className="flex flex-wrap gap-2">
+                                <button type='submit' className="self-center py-1.5 px-2 sm:py-1.5 sm:px-4 border bg-red-600 rounded-3xl text-white">Aceptar</button>
+                                <button type="button" className="py-1 px-2 sm:py-1.5 sm:px-4 self-center border-black border rounded-3xl" onClick={() => navigate(`/backoffice/${route}`)}>Cancelar</button>
+                            </div>
+                        </Form>
+                    </div>
+                )}
+            </Formik>
+        </>
+    )
+};
+
+const generateInputs = (value, handleChange) => {
+    switch (value[0]) {
+        case 'name':
+            return (
+                <TextField value={value} handleChange={handleChange} key={value[0]}/>
+            )
+        case 'content':
+            return (
+                <ContentField value={value} handleChange={handleChange} key={value[0]}/>
+            )    
+        default:
+            break;
+    }
+}
+
+export default BackofficeForm;
