95a7f185a896f2112f34f3829cde48e561513154
leandro <martinezleandro2004@gmail.com>
2022-09-19 16:06:15 -0300

    creacion y edicion de testimonios añadidos

--- src/pages/Backoffice/Testimonials/TestimonialsBackoffice.js
+++ src/pages/Backoffice/Testimonials/TestimonialsBackoffice.js
@@ -1,35 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from '../../../Components/Table/Table';
-import testimonialsData from '../../../mocks/testimonialsMock';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import BackofficeForm from "../../../Components/DynamicForm/BackofficeForm";
+import APICalls from "../../../shared/APICalls";
 
 const TestimonialsBackoffice = () => {
-    return (
-        <>
-            <Table
-                title='testimonios'
-                tableHeader={['Nombre', 'Contenido']}
-                tableData={testimonialsData}
-                requiredProperties={['name', 'content', 'createdAt']}
-                buttons={[
-                    { type: 'Editar', handler: handleEdit },
-                    { type: 'Eliminar', handler: handleDelete }
-                ]}
-            />
-            <button className='bg-blue-500 hover:bg-blue-400 pt-4 pb-4 pl-8 pr-8 shadow-xl text-white font-semibold flex m-auto' onClick={handleCreate}>Agregar testimonio</button>
-        </>
-    )
-}
+  const [testimonials, setTestimonials] = useState([]);
+  const navigate = useNavigate();
 
-const handleEdit = () => {
-    console.log('edit')
+  useEffect(() => {
+    const getTestimonials = async () => {
+      const res = await APICalls.get('/testimonials')
+      const { data } = res
+      const { allTestimonials } = data
+      setTestimonials(allTestimonials)
+    }
+    getTestimonials();
+  }, [])
+
+  const testimonialsData = {
+    name: '',
+    content: '',
+    image: ''
+  }
+  const handleCreate = () => {
+    navigate('create', {
+      state: {
+        fields: testimonialsData,
+        method: 'POST',
+        route: 'testimonials',
+        title: 'Crear Testimonial'
+      }
+    })
+  }
+  const handleEdit = (data) => {
+    navigate('edit', {
+      state: {
+        fields: testimonialsData,
+        data,
+        method: 'PUT',
+        route: 'testimonials',
+        title: 'Editar Testimonial'
+      }
+    })
+  }
+  return (
+    <Routes>
+      <Route path="/" element={
+        <>
+          <Table
+            title='testimonios'
+            tableHeader={['Nombre', 'Contenido']}
+            tableData={testimonials}
+            requiredProperties={['name', 'content', 'createdAt']}
+            buttons={[
+              { type: 'Editar', handler: handleEdit },
+              { type: 'Eliminar', handler: handleDelete }
+            ]}
+          />
+          <button className='bg-blue-500 hover:bg-blue-400 pt-4 pb-4 pl-8 pr-8 shadow-xl text-white font-semibold flex m-auto' onClick={handleCreate}>Agregar testimonio</button>
+        </>
+      } />
+      <Route path='/create' element={<BackofficeForm />} />
+      <Route path='/edit' element={<BackofficeForm />} />
+    </Routes>
+  )
 }
 
 const handleDelete = () => {
-    console.log('delete')
-}
-
-const handleCreate = () => {
-    console.log('create')
+  console.log('delete')
 }
 
 export default TestimonialsBackoffice;
